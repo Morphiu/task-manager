76e9ea3016b7ef442e5c544dedd73fb23fffd7fb
Mike Mghenyi <mikemghenyi5@gmail.com>
2025-03-06 22:28:45 +0300

    Simple task manager using MERN stack

---
+++ frontend/task-manager/src/components/TaskItem.jsx
@@ -0,0 +1,35 @@
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
+import {useParams} from 'react-router-dom';
+
+//Fetching task
+function TaskItem(){
+    const [task, setTask] = useState(null);
+    const {id} = useParams();
+
+    useEffect(()=>{
+        fetchTask();
+    }, [id]);
+
+    const fetchTask = async () => {
+        try{
+            const response = await axios.get(`http://localhost:5000/api/task/${id}`);
+            setTask(response.data);
+        } catch(error){
+            console.error("Error fetching Task:", error);
+        }
+    }
+
+    if (!task) return <div>Loading...</div>
+
+    return(
+        <div>
+            <h2 className="text-2xl font-bold mb-4">{task.title}</h2>
+            <p>Description: {task.description}</p>
+            <p>Status: {task.status}</p>
+            <p>Due Date: {task.dueDate ? task.dueDate.slice(0, 10) : 'N/A'}</p>
+        </div>
+    );
+}
+
+export default TaskItem
